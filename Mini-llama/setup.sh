0f0f69dee033f8d199414ccbfdbe32c5c4d22525
Quanchao <quanchao@MacBookPro.lan>
2025-06-30 00:24:12 -0700

    Fixed the commands for macbook

--- Mini-llama/setup.sh
+++ Mini-llama/setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-conda create -n llama_hw python=3.11
-conda activate llama_hw
+conda create -n llama_hw1_4 python=3.11
+conda activate llama_hw1_4
 
 # Modify this command depending on your system's environment.
 # As written, this command assumes you have CUDA on your machine, but
 # refer to https://pytorch.org/get-started/previous-versions/ for the correct
 # command for your system.
-conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=11.8 -c pytorch -c nvidia
+#conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=11.8 -c pytorch -c nvidia
+conda install pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 -c pytorch -c nvidia
 pip install tqdm==4.66.1
 pip install requests==2.31.0
 pip install importlib-metadata==3.7.0
@@ -15,5 +16,5 @@ pip install filelock==3.0.12
 pip install scikit-learn==1.2.2
 pip install numpy==1.26.3
 pip install tokenizers==0.13.3
-pip install sentencepiece==0.1.9
+pip install sentencepiece==0.2.0
 #wget https://www.cs.cmu.edu/~vijayv/stories42M.pt
